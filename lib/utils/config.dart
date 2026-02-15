48d278419d30e412299ca25b6a4e14cac4203ae6
JoachimTislov <joachim.tislov@gmail.com>
2026-02-15 22:29:55 +0100

    chore(config): update email (gmail -> example)
    - can apparently be problematic...

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -4,6 +4,6 @@ class Config {
   // isDevelopment is default when appFlavor isn't set
   static const bool isDevelopment = appFlavor == null;
   static const String? environment = isDevelopment ? 'development' : appFlavor;
-  static const String testUserEmail = 'bob@gmail.com';
+  static const String testUserEmail = 'bob@example.com';
   static const String testUserPassword = 'password123';
 }
