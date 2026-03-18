83defb4d5f809cc5d7cbaac2e466656d0f35cae4
JoachimTislov <joachim.tislov@gmail.com>
2026-03-18 21:26:07 +0100

    format with dart

--- lib/widgets/product/product_list_item.dart
+++ lib/widgets/product/product_list_item.dart
@@ -13,7 +13,9 @@ class ProductListItem extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       child: ListTile(
         title: Text(product.name),
-        subtitle: product.description.isNotEmpty ? Text(product.description) : null,
+        subtitle: product.description.isNotEmpty
+            ? Text(product.description)
+            : null,
         // Add more details or actions here if needed
       ),
     );
