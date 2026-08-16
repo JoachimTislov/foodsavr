5d6707e15803140f2551ee8e72082d5a99a4f469
JoachimTislov <joachim.tislov@gmail.com>
2026-08-16 19:08:33 +0200

    style: sort imports

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:foodsavr/services/collection_service.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injectable/injectable.dart';
-import 'package:foodsavr/services/collection_service.dart';
 
 import '../interfaces/i_auth_service.dart';
 
